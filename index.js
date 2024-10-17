cfebc79f8b560ddc196cfc4372614e37ba4740b5
FilipposDe <philipposdem@hotmail.com>
2024-10-17 19:39:30 +0300

    Refactor import statement in index.js and helpers/index.js

--- index.js
+++ index.js
@@ -123,6 +123,10 @@ app.post('/webhooks', async (req, res) => {
 	}
 })
 
+app.post('/health', async (req, res) => {
+	res.sendStatus(200)
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
